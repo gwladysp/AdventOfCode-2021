b3a231530981cc39ce619a2aa73d1e309daef0b5
Gwladys <gwladys.pinel54@gmail.com>
2021-12-01 23:34:13 +0100

    feat: add day1 part2 soluce

--- day1/count.js
+++ day1/count.js
@@ -1,6 +1,7 @@
 // run this code in the console!
 const values = document.querySelector("pre").innerText.split("\n");
 countIncreasements(values)
+countIncreasementsWithSum(values)
 
 /**
  * Counts the number of times a depth measurement increases from the previous measurement
@@ -13,4 +14,21 @@ function countIncreasements(input) {
         }
     })
     console.log(increasementsCounter + " increasements!");
+}
+
+/**
+ * Counts the number of times a depth three-measurements sum increases from the previous three-measurements sum
+ */
+function countIncreasementsWithSum(input) {
+    let increasementsCounter = 0;
+    let previousSum = 0;
+    let sum = 0;
+    input.map((value, index) => {
+        sum = parseInt(value) + parseInt(input[index + 1]) + parseInt(input[index + 2])
+        if (sum > previousSum && index > 0) {
+            increasementsCounter++;
+        }
+        previousSum = sum;
+    })
+    console.log(increasementsCounter + " increasements with three-measurements sums!");
 }
